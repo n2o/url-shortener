fbd0932420b9332a61043ddb751206c3ae823886
Christian Meter <cmeter@googlemail.com>
2019-11-26 15:54:42 +0100

    Refactor LinkController

--- src/main/java/de/hhu/propra/link/controllers/LinkController.java
+++ src/main/java/de/hhu/propra/link/controllers/LinkController.java
@@ -1,7 +1,7 @@
 package de.hhu.propra.link.controllers;
 
 import de.hhu.propra.link.entities.Link;
-import de.hhu.propra.link.repositories.LinkRepository;
+import de.hhu.propra.link.services.LinkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,18 +17,18 @@ import java.util.Optional;
 @Controller
 @SessionScope
 public class LinkController {
-    private final LinkRepository linkRepository;
+    private final LinkService linkService;
     private String errorMessage;
     private String successMessage;
     private Link currentLink = new Link();
 
-    public LinkController(LinkRepository linkRepository) {
-        this.linkRepository = linkRepository;
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("links", linkRepository.findAll());
+        model.addAttribute("links", linkService.allLinks());
         model.addAttribute("link", currentLink);
         model.addAttribute("error", errorMessage);
         model.addAttribute("success", successMessage);
@@ -40,17 +40,17 @@ public class LinkController {
         this.currentLink = link;
 
         // suggest an abbreviation if not set
-        if(link.getAbbreviation().isEmpty()){
-            String abbrevation = makeAbbrevation(new String(link.getUrl()));
-            link.setAbbreviation(abbrevation);
+        if (link.getAbbreviation().isEmpty()) {
+            linkService.createAbbreviation(link);
+
         }
 
         if (bindingResult.hasErrors()) {
             return "index";
         }
 
-        if (linkRepository.findById(link.getAbbreviation()).isEmpty()) {
-            linkRepository.save(link);
+        if (linkService.findById(link.getAbbreviation()).isEmpty()) {
+            linkService.save(link);
             setMessages(null, "Successfully added a new short link!");
             this.currentLink = new Link();
         } else {
@@ -62,15 +62,15 @@ public class LinkController {
     @GetMapping("/{abbreviation}")
     public String redirectUrl(@PathVariable String abbreviation) {
         setMessages(null, null);
-        Optional<Link> link = linkRepository.findById(abbreviation);
+        Optional<Link> link = linkService.findById(abbreviation);
         return link.map(value -> "redirect:" + value.getUrl()).orElse("redirect:/");
     }
 
     @PostMapping("/{abbreviation}/delete")
     public String deleteLink(@PathVariable String abbreviation) {
-        Optional<Link> link = linkRepository.findById(abbreviation);
+        Optional<Link> link = linkService.findById(abbreviation);
         if (link.isPresent()) {
-            linkRepository.delete(link.get());
+            linkService.delete(link.get());
             setMessages(null, "Successfully deleted short link");
         } else {
             setMessages("Short link could not be deleted, because it was not found in the database", null);
@@ -87,77 +87,5 @@ public class LinkController {
     private void setMessages(String errorMessage, String successMessage) {
         this.errorMessage = errorMessage;
         this.successMessage = successMessage;
-    }
-
-    /**
-     * creates a not yet used viable abbrevation for the entered url
-     * @param url The URL that is to be shortened
-     * @return the abbrevation for the long URL
-     */
-    private String makeAbbrevation(String url){
-
-        // removing the scheme from the URL
-        // "https://example.com" -> "example.com"
-        int schemelessUrlindex = url.indexOf("://");
-        String schemelessUrl = url;
-        if(schemelessUrlindex > 0) {
-            schemelessUrl = url.substring(schemelessUrlindex + 3, url.length());
-        } else
-            return url; // URL possibly not correct, so just return it
-
-        if(schemelessUrl.indexOf("www") == 0){
-            schemelessUrl = schemelessUrl.substring(3);
-        }
-
-        // determine a suitable abbreviation taking the paths in consideration
-        // ex: "sub.example.com/path/to/index.html" -> "sbxmplpti"
-        String[] domainAndPaths = schemelessUrl.split("/");
-        String tmp_abbreviation = unvowelize(domainAndPaths[0]);
-        for(int i = 1; i < domainAndPaths.length; i++){
-            tmp_abbreviation += domainAndPaths[i].charAt(0);
-        }
-
-        // create another abbreviation if the preferred one already exists
-        int i = 1;
-        String abbrevation = tmp_abbreviation;
-        while( ! linkRepository.findById(abbrevation).isEmpty() ){
-            abbrevation = new String(tmp_abbreviation + i);
-            i += 1;
-        }
-
-        return abbrevation;
-    }
-
-    /**
-     * @param domain The domain name where the vowels, dots and the TLD should be removed from
-     * @return the String without any vowels, dots and the TLD
-     */
-    private String unvowelize(String domain) {
-        String unvowelized = "";
-
-        for(int i = 0; i < domain.lastIndexOf('.'); i++){
-            if(! (isVowel(domain.charAt(i)) || domain.charAt(i) == '.') ){
-                unvowelized += domain.charAt(i);
-            }
-        }
-
-        return unvowelized;
-    }
-
-    /**
-     * returns true if entered letter is a vowel, else false.
-     * a, e, i, o, u are vocals.
-     * @param letter The letter to check
-     * @return true if letter is a vowel, false elsewise
-     */
-    private boolean isVowel(char letter) {
-        switch(letter){
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u': return true;
-            default:  return false;
-        }
     }
 }
---
+++ src/main/java/de/hhu/propra/link/services/LinkService.java
@@ -0,0 +1,132 @@
+package de.hhu.propra.link.services;
+
+import de.hhu.propra.link.entities.Link;
+import de.hhu.propra.link.repositories.LinkRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class LinkService {
+    private final LinkRepository linkRepository;
+
+    public LinkService(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    /**
+     * Creates a not yet used viable abbreviation for the entered URL.
+     *
+     * @param url The URL that is to be shortened
+     * @return the abbrevation for the long URL
+     */
+    private String makeAbbrevation(String url) {
+        String schemelessUrl = removeProtocol(url);
+        if (schemelessUrl.indexOf("www") == 0) {
+            schemelessUrl = schemelessUrl.substring(3);
+        }
+        StringBuilder abbreviation = makeAbbreviationFromPath(schemelessUrl);
+        return findNextFreeAbbreviation(abbreviation);
+    }
+
+    /**
+     * Remove leading http(s?):// from URL.
+     *
+     * @param url current url
+     * @return (possibly) stripped url
+     */
+    private String removeProtocol(String url) {
+        int schemelessUrlindex = url.indexOf("://");
+        if (schemelessUrlindex > 0) {
+            return url.substring(schemelessUrlindex + 3);
+        } else {
+            return url;
+        }
+    }
+
+    /**
+     * Determine a suitable abbreviation taking the paths into consideration.
+     * ex: "sub.example.com/path/to/index.html" -> "sbxmplpti"
+     *
+     * @param schemelessUrl part of URL
+     * @return current abbreviation
+     */
+    private StringBuilder makeAbbreviationFromPath(String schemelessUrl) {
+        String[] domainAndPaths = schemelessUrl.split("/");
+        StringBuilder tmp_abbreviation = new StringBuilder(unvowelize(domainAndPaths[0]));
+        for (int i = 1; i < domainAndPaths.length; i++) {
+            tmp_abbreviation.append(domainAndPaths[i].charAt(0));
+        }
+        return tmp_abbreviation;
+    }
+
+    /**
+     * Create another abbreviation if the preferred one already exists
+     *
+     * @param tmp_abbreviation current abbreviation
+     * @return abbreviation with appended number for uniqueness
+     */
+    private String findNextFreeAbbreviation(StringBuilder tmp_abbreviation) {
+        int i = 1;
+        String abbrevation = tmp_abbreviation.toString();
+        while (linkRepository.findById(abbrevation).isPresent()) {
+            abbrevation = tmp_abbreviation.toString() + i;
+            i += 1;
+        }
+        return abbrevation;
+    }
+
+    /**
+     * @param domain The domain name where the vowels, dots and the TLD should be removed from
+     * @return the String without any vowels, dots and the TLD
+     */
+    private String unvowelize(String domain) {
+        StringBuilder unvowelized = new StringBuilder();
+        for (int i = 0; i < domain.lastIndexOf('.'); i++) {
+            if (!(isVowel(domain.charAt(i)) || domain.charAt(i) == '.')) {
+                unvowelized.append(domain.charAt(i));
+            }
+        }
+        return unvowelized.toString();
+    }
+
+    /**
+     * Check if character is a vowel.
+     *
+     * @param letter The letter to check
+     * @return true if letter is a vowel, false elsewise
+     */
+    private boolean isVowel(char letter) {
+        switch (letter) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public Iterable<Link> allLinks() {
+        return linkRepository.findAll();
+    }
+
+    public Optional<Link> findById(String abbreviation) {
+        return linkRepository.findById(abbreviation);
+    }
+
+    public void delete(Link link) {
+        linkRepository.delete(link);
+    }
+
+    public void save(Link link) {
+        linkRepository.save(link);
+    }
+
+    public void createAbbreviation(Link link) {
+        String abbrevation = makeAbbrevation(link.getUrl());
+        link.setAbbreviation(abbrevation);
+    }
+}
