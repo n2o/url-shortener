4ff72fe3dbf9799d04474bfda6b6d0a6f674fb03
Marcel <marcel.haas@hhu.de>
2020-01-17 09:53:54 +0100

    ADD: tests (#10)
    * ADD: test for index
    ADD: test for new link
    
    * Update src/test/java/de/hhu/propra/link/controllers/LinkControllerTest.java
    
    Co-Authored-By: iTitus <iTitus@users.noreply.github.com>
    
    * FIX: previous code mess
    
    Co-authored-by: iTitus <iTitus@users.noreply.github.com>

---
+++ src/test/java/de/hhu/propra/link/controllers/LinkControllerTest.java
@@ -0,0 +1,38 @@
+package de.hhu.propra.link.controllers;
+
+import de.hhu.propra.link.services.LinkService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class LinkControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    LinkService linkService;
+
+    @Test
+    void testIndex() throws Exception {
+        mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testNewLink() throws Exception {
+        mvc
+                .perform(
+                        post("/")
+                                .param("abbreviation", "abc")
+                                .param("url", "http://www.abc.de")
+                )
+                .andExpect(status().is3xxRedirection());
+    }
+
+}
